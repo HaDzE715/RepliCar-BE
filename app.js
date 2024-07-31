be2858db1dfdbcd9bb3ef173721925ce42a27fd4
HaDzE7 <hadebayaa@gmail.com>
2024-08-01 02:20:29 +0300

    Updated EmailTemplate

--- app.js
+++ app.js
@@ -48,20 +48,47 @@ app.post("/subscribe", async (req, res) => {
   }
 
   try {
-    await appendToSheet(email);
-
-    // Send confirmation email
     await transporter.sendMail({
       from: `"Replicar" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Thank you for subscribing!",
       html: emailTemplate,
+      attachments: [
+        {
+          filename: "logo.jpg",
+          path: "./Pictures/logo.jpg", // Replace with your image file path
+          cid: "logo", // same cid value as in the html img src
+        },
+        {
+          filename: "collection.jpg",
+          path: "./Pictures/collection.jpg", // Replace with your image file path
+          cid: "collection",
+        },
+        {
+          filename: "product.jpg",
+          path: "./Pictures/Tiffany1.jpeg", // Replace with your image file path
+          cid: "product",
+        },
+        {
+          filename: "footerLogo.jpg",
+          path: "./Pictures/logo.jpg", // Replace with your image file path
+          cid: "footerLogo",
+        },
+        {
+          filename: "instalogo.png",
+          path: "./Pictures/instalogo.png", // Replace with your image file path
+          cid: "instalogo",
+        },
+        {
+          filename: "emaillogo.png",
+          path: "./Pictures/EmailLogo.png", // Replace with your image file path
+          cid: "emaillogo",
+        },
+      ],
     });
-
-    res.status(200).send("Subscription successful");
+    console.log("Email sent successfully");
   } catch (error) {
-    console.error("Error appending to sheet:", error);
-    res.status(500).send("Error subscribing to newsletter");
+    console.error("Error sending email:", error);
   }
 });
 
