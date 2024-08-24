8adb080a76913ac0000e2f50f8a1bb9ebeed20d1
HaDzE7 <hadebayaa@gmail.com>
2024-08-24 15:01:09 +0300

    Added

--- server.js
+++ server.js
@@ -116,6 +116,6 @@ process.on("SIGINT", async () => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
